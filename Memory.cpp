1565693cffedcdad3d66429298eec0ed65a07bba
Andrew Niklas <acniklas@cpp.edu>
2016-01-27 19:08:08 -0800

    3D allocation done

--- Memory.cpp
+++ Memory.cpp
@@ -2,6 +2,14 @@
 
 int*** alloc3D(int x, int y, int z){
 	
+	int*** result = new int**[x];
+	for (int i = 0; i < x; ++i){
+		result[i] = new int*[y];
+		for (int j = 0; j < y; j++){
+			result[j] = new int[z];
+		}
+	}
+	return result;
 }
 
 
